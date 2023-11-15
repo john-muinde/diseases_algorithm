969017a1a31b323fd3d3699c2ad3d8bea4eea793
john-muinde <66753112+john-muinde@users.noreply.github.com>
2023-11-15 20:04:01 +0300

    js implementation

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ function extractSymptoms(text, symptoms) {
     symptom = symptom.toLowerCase();
     for (let word of words) {
       const match = stringSimilarity.findBestMatch(symptom, [word]);
-      if (match.bestMatch.rating >= 0.1) {
+      if (match.bestMatch.rating >= 0.8) {
         extractedSymptoms[symptom] = match.bestMatch.rating;
       }
     }
